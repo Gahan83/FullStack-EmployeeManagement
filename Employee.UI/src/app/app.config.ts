cec9f2065e8644186268242853bb42669b05adfb
Gahan Kotebagilu <Gahan.Kotebagilu@anthology.com>
2025-04-22 14:38:53 +0530

    Implement employee management features with CRUD operations and UI updates

--- Employee.UI/src/app/app.config.ts
+++ Employee.UI/src/app/app.config.ts
@@ -2,8 +2,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: 
+  [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient(),
+  ]
 };
